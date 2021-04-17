dffc3215815de90f623e866f688b929c64e49d2e
dbonach <deivitlopes@gmail.com>
2021-04-16 21:48:14 -0300

    Create routes for articles in the form /wiki/TITLE
    This concludes cs50web project-1 "Entry Page" requirements,
    it creates routes to render a page for an requested articles.
    Also an "error" page was created to be shown when a requested
    article does not exist.
    
    Official requirements:
    Entry Page: Visiting /wiki/TITLE, where TITLE is the title of an
    encyclopedia entry, should render a page that displays the contents of
    that encyclopedia entry.
    
     - The view should get the content of the encyclopedia entry by calling
       the appropriate util function.
     - If an entry is requested that does not exist, the user should be
       presented with an error page indicating that their requested page was
       not found.
     - If the entry does exist, the user should be presented with a page
       that displays the content of the entry. The title of the page should
       include the name of the entry.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # path("wiki/CSS", views.wiki, name="css")
+    # path("<char:title>", views.wiki, name="css")
+    re_path(r'^wiki/(\w+)/$', views.wiki, name='entry')
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import re
+import markdown2
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +11,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, entry):
+    # print("###", end=" ")
+    # print(request.path)
+    # entry = re.sub("/wiki/", "", request.path)
+    # print(entry)
+    content = util.get_entry(entry)
+    # print(entry + "- wiki")
+    if (content == None):
+        return render(request, "encyclopedia/notfound.html", {
+            "entry": entry + " - wiki"
+        })
+    # print(markdown2.markdown(text))
+    return render(request, "encyclopedia/wiki.html", {
+        "entry": entry + " - wiki",
+        "content": markdown2.markdown(content)
+    })
