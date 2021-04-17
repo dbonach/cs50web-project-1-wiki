c6e2a9cf4bdeab5ce77f1d9d97d13a7bc93b0892
dbonach <deivitlopes@gmail.com>
2021-04-17 01:41:17 -0300

    Add links to listed articles on index page
    This concludes "Index Page" requirements.
    
    Official requirement:
    Index Page: Update index.html such that, instead of merely listing the
    names of all pages in the encyclopedia, user can click on any entry name
    to be taken directly to that entry page.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 
 from . import views
 
+app_name = 'encyclo'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>/", views.wiki, name="entry")
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -12,18 +12,13 @@ def index(request):
     })
 
 def wiki(request, entry):
-    # print("###", end=" ")
-    # print(request.path)
-    # entry = re.sub("/wiki/", "", request.path)
-    print(entry)
     content = util.get_entry(entry)
-    # print(entry + "- wiki")
     if (content == None):
         return render(request, "encyclopedia/notfound.html", {
             "entry": entry + " - Wiki"
         })
-    # print(markdown2.markdown(text))
-    return render(request, "encyclopedia/wiki.html", {
-        "entry": entry + " - Wiki",
-        "content": markdown2.markdown(content)
+    else:
+        return render(request, "encyclopedia/wiki.html", {
+            "entry": entry + " - Wiki",
+            "content": markdown2.markdown(content)
     })
