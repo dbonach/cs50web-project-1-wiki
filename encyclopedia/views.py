eef8b4365423e0eeb0f68c3ab550a63ba48f669b
dbonach <deivitlopes@gmail.com>
2021-04-17 21:06:29 -0300

    Add response to a query search on the search box

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,23 +2,43 @@ from django.shortcuts import render
 import re
 import markdown2
 from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from . import util
 
 
 def index(request):
+
+    if(request.GET):
+        query = request.GET['q']
+        if(query):
+            return HttpResponseRedirect(reverse("encyclo:entry", kwargs={'entry': query}))
+        else:
+            return HttpResponseRedirect(reverse("encyclo:index"))
+        
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def wiki(request, entry):
+
+    if(request.GET):
+        query = request.GET['q']
+        if(query):
+            return HttpResponseRedirect(reverse("encyclo:entry", kwargs={'entry': query}))
+        else:
+            return HttpResponseRedirect(reverse("encyclo:entry", kwargs={'entry': entry}))
+
     content = util.get_entry(entry)
+
     if (content == None):
         return render(request, "encyclopedia/notfound.html", {
-            "entry": entry + " - Wiki"
+            "title": entry + " - Wiki",
+            "query": entry
         })
     else:
         return render(request, "encyclopedia/wiki.html", {
-            "entry": entry + " - Wiki",
+            "title": entry + " - Wiki",
             "content": markdown2.markdown(content)
     })
