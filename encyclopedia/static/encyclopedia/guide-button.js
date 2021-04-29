57d855cc38f0b9a5d78c262433e65937578c06b7
dbonach <deivitlopes@gmail.com>
2021-04-29 16:16:11 -0300

    Add animation and change color
    An animation was added to the bottom visible when the page is in small
    screen mode. That animation provides a smooth transition to show or hide
    the menu.
    
    Sidebar background color was changed to green.

--- encyclopedia/static/encyclopedia/guide-button.js
+++ encyclopedia/static/encyclopedia/guide-button.js
@@ -1,3 +1,7 @@
 document.getElementById("guide-button").addEventListener("click", function () {
-  document.getElementById("guide-content").classList.toggle("hide-guide");
+
+  if (document.getElementById("guide-content").classList.length == 2) {
+    document.getElementById("guide-content").classList.toggle("hide-guide");
+  }
+  document.getElementById("guide-content").classList.toggle("show-guide");
 })
